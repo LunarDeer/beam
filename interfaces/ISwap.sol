e3e26cf652f05dc70a2f364134d19bd0b297d02d
LunarDeer <lunarbeam@protonmail.com>
2021-09-18 07:19:05 +0300

    Initial commit

---
+++ interfaces/ISwap.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.6.0;
+
+interface ISwap {
+    function WETH() external pure returns (address);
+    
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+    
+    function removeLiquidityETH(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountToken, uint amountETH);
+    
+    function removeLiquidityETHSupportingFeeOnTransferTokens(
+      address token,
+      uint liquidity,
+      uint amountTokenMin,
+      uint amountETHMin,
+      address to,
+      uint deadline
+    ) external returns (uint amountETH);
+    
+    function swapExactETHForTokens(
+        uint amountOutMin, 
+        address[] calldata path, 
+        address to, 
+        uint deadline
+    ) external payable returns (uint[] memory amounts);
+}
